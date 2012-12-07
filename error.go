2a35d0de0467d5b41206327a17e66faa2990fe9d
sevlyar <sergey.yarmonov@gmail.com>
2012-12-07 10:26:16 +0700

    Переработана подсистема исключений

--- error.go
+++ error.go
@@ -1,22 +1,31 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"log"
+	"os"
 )
 
+// throw вызывает panic с указанным сообщением, помещенным в error.
 func throw(f string, a ...interface{}) {
-	panic(errors.New(fmt.Sprintf(f, a...)))
+	panic(fmt.Errorf(f, a...))
 }
 
-func catch() {
+// rethrow прекращает panic, извлекает сообщение, добавляет к нему префикс
+// и снова вызывает panic(). Функция должна вызываться только с помощью defer.
+func rethrow(f string, a ...interface{}) {
 	if err := recover(); err != nil {
-		fmt.Println(err)
+		throw(f+": "+fmt.Sprint(err), a...)
 	}
 }
 
-func rethrow(f string, a ...interface{}) {
+// exit отлавливает panic, выводит сообщение в stdout и stderr? и завершает
+// работу программы с кодом 1. Если не было panic, то ничего не делает.
+// Должна вызываться только с помощью defer и из main().
+func exit() {
 	if err := recover(); err != nil {
-		throw(fmt.Sprint(err)+": "+f, a...)
+		log.Println(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
